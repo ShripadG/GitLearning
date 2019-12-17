41aa6f83b46ca5300e26401a1fa1ebfda24ff572
Shripad gavaraskar <sgavaraskar@gmail.com>
2019-12-17 12:25:42 +0530

    Add project files.

---
+++ GitSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World with my first test latersdfsdffdsfsdsd!");
+        }
+    }
+}
